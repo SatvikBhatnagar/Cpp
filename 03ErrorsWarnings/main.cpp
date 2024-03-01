b44aba8dc633d40cd20aa3cde8125d24745d12c0
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2024-03-01 22:32:31 +0530

    warnings example (run the code in vscode terminal and linux terminal to see runtime errors)
    Signed-off-by: stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>

--- 03ErrorsWarnings/main.cpp
+++ 03ErrorsWarnings/main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    //Compile time error
-    std::cout << "Hello, World" << std::endl
+    // Compile time error
+    std::cout << "Hello, World" << std::endl;
+
+    // Run time error
+    int value = 7 / 0;
+    std::cout << "value : " << value << std::endl;
     return 0;
 }
